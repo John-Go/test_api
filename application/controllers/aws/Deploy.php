efca69a1972670bf6905c659a467cd6ae9d4250e
John-Go <gogwangbok@gmail.com>
2017-03-28 15:05:45 +0900

    Return json

--- application/controllers/aws/Deploy.php
+++ application/controllers/aws/Deploy.php
@@ -18,6 +18,8 @@ class Deploy extends CI_Controller {
         //
         $val = shell_exec('sh /gfdata/update.sh pull');
         log_message('error', 'Api server : '.print_r($val,true));
+
+        return $this->output->set_content_type('application/json')->set_output(json_encode($val));
     }
 
     public function liveExecuteCommand($cmd){
